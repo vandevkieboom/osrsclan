51aa3b786d420f9a53dcd18c3dd28b50c8600e56
Jordy Van Den Kieboom <vandevkieboom@gmail.com>
2026-05-14 02:04:51 +0200

    Added analytics for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import { ClanRankings } from "./page/time-served-page.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClanRankings />
+    <Analytics />
   </StrictMode>,
 );
